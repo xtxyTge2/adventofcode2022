6bc3b54e70d830bca037d2a9c289d6f46afaf214
xtxyTge2 <f1AZnn@protonmail.com>
2022-12-03 11:18:58 +0100

    Added empty day03 folder. Created template python file.

---
+++ template.py
@@ -0,0 +1,15 @@
+def first_part(input_file):
+    return None
+
+def second_part(input_file):
+    return None
+
+input_file_name = ""
+
+with open(input_file_name) as input_file:
+    result_first = first_part(input_file)
+    print("Result of the first part is: {}".format(result_first))
+
+with open(input_file_name) as input_file:
+    result_second = second_part(input_file)
+    print("Result of the second part is: {}".format(result_second))

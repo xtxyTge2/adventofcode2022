86f02c22dba2b766061ef2ca7893a9344dc3add9
xtxyTge2 <f1AZnn@protonmail.com>
2022-12-05 11:47:22 +0100

    Completed day05 assignment. Modified template file.

--- day05/day05.py
+++ day05/day05.py
@@ -1,89 +1,131 @@
-"""
-Interval class. Does allow __non-valid__ interval objects where its right boundary
-is smaller than its left boundary.
-"""
-class interval:
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
-
-    def is_contained_in(self, other):
-        return self.left <= other.left and self.right >= other.right
+class CrateMoveOrder():
+    def __init__(self, num_objects, src_crate, dest_crate):
+        self.num_objects = num_objects
+        self.src_crate = src_crate
+        self.dest_crate = dest_crate
 
     def __repr__(self):
-        return "Interval object: ({}, {})".format(self.left, self.right)
+        return "CrateMoveOrder: Number of objects to move: {}, Source: {}, Destination: {}"\
+                .format(self.num_objects, self.src_crate, self.dest_crate)
+    
+class CrateStockpile():
+    def __init__(self, num_crates, crates):
+        self.num_crates = num_crates
+        self.crates = crates
 
-    def has_overlap_with(self, other):
-        intersection = self.get_intersection(other)
-        return not intersection.is_empty()    
+    def execute_move_order(self, move_order, reverse_crates_when_moving: False):
+        num_objects = move_order.num_objects
+        src_id = move_order.src_crate
+        dest_id = move_order.dest_crate
+        if num_objects <= 0 \
+                or src_id < 0 or src_id > self.num_crates \
+                or dest_id < 0 or dest_id > self.num_crates: 
+                    return 
 
-    def get_intersection(self, other):
-        left = max(self.left, other.left)
-        right = min(self.right, other.right)
-        return interval(left, right)
-
-    def is_empty(self):
-        return self.left > self.right
-
-
-"""
-Parse the input line by line. Split each line into its two parts and parse 
-the left and right hand side into interval objects. For each line put 
-the left and right side interval object into a tuple and add this tuple into
-a list. Returns this list.
-"""
-def parse_input(input_file):
-    data = []
-    for line in input_file:
-        split = line.rstrip().split(",")
-
-        a = split[0].split("-")
-        b = split[1].split("-")
-
-        a_left = int(a[0])
-        a_right = int(a[1])
-
-        b_left = int(b[0])
-        b_right = int(b[1])
-
-        x = interval(a_left, a_right)
-        y = interval(b_left, b_right)
+        src = self.crates[src_id]
+        dest = self.crates[dest_id]
+        objects_to_move = src[-num_objects:]
         
-        data.append((x, y))
-    return data
+        self.crates[src_id] = src[:-len(objects_to_move) or None]
+        if reverse_crates_when_moving: 
+            self.crates[dest_id] = dest + list(reversed(objects_to_move))
+        else:
+            self.crates[dest_id] = dest + list(objects_to_move)
 
-"""
-Helper wrapper function. Unwraps a tuple object containing two interval objects 
-and checks if one is contained in the other.
-"""
-def one_contains_the_other(interval_tuple):
-    x = interval_tuple[0]
-    y = interval_tuple[1]
-    return x.is_contained_in(y) or y.is_contained_in(x)
 
-"""
-Helper wrapper function. Unwraps a tuple object containing two interval objects
-and checks if these two intervals have non-empty overlap.
-"""
-def has_overlap(interval_tuple):
-    x = interval_tuple[0]
-    y = interval_tuple[1]
-    return x.has_overlap_with(y)
 
-def first_part(data):
-    return len(list(filter(lambda t: one_contains_the_other(t), data)))
+    def __repr__(self):
+        string_repr = []
+        string_repr.append("CrateStockpile object: number of crates: {}"\
+                .format(self.num_crates))
+        for i, crate in enumerate(self.crates):
+            string_repr.append("{}: ".format(i) + str(crate))
+        return "\n".join(string_repr)
 
-def second_part(input_file):
-    return len(list(filter(lambda t: has_overlap(t), data))) 
+
+def parse_input(input_file):
+    crate_position_lines = []
+    move_order_lines = []
+    
+
+    crate_definition_line = ""
+    parsed_crate_definition_line = False
+    parsing_crate_positions = True
+    for line in input_file:
+        if not parsed_crate_definition_line and "1" in line:
+            crate_definition_line = line
+            parsed_crate_definition_line = True
+            parsing_crate_positions = False
+            continue
+        if parsing_crate_positions:
+            crate_position_lines.append(line)
+        
+        if "move" in line:
+            move_order_lines.append(line)
+
+
+    # parse crate data
+    crate_ids = crate_definition_line
+    crate_position_index = []
+    for i, c in enumerate(crate_ids):
+        if c.isalnum():
+            crate_position_index.append((i, int(c)))
+
+    crates_raw = []
+    num_of_crates = len(crate_position_index)
+    for (i, v) in crate_position_index:
+        current_crate = []
+        for line in reversed(crate_position_lines):
+            if line[i].strip():
+                current_crate.append(line[i])
+        crates_raw.append(current_crate)
+    crate_stockpile = CrateStockpile(num_of_crates, crates_raw) 
+    
+    # parse move orders
+    move_orders = []
+    for line in move_order_lines:
+        line_split = line.rstrip().split(" ")
+        num_objects = int(line_split[1])
+        src_crate = int(line_split[3]) - 1 # crates are 0 index based internally
+        dest_crate = int(line_split[5]) - 1 # crates are 0 index based interally
+
+        move_orders.append(CrateMoveOrder(num_objects, src_crate, dest_crate))
+    
+    return (crate_stockpile, move_orders)
+
+def execute_move_orders(data, show_execution: False, reverse_move_order_when_moving):
+    crate_stockpile = data[0]
+    move_orders = data[1]
+    if show_execution:
+        print(crate_stockpile)
+    for move_order in move_orders:
+        if show_execution:
+            print(move_order)
+            print(crate_stockpile)
+            print("".join(["#" for i in range(80)]))
+        crate_stockpile.execute_move_order(move_order, reverse_move_order_when_moving)
+    
+    if show_execution:
+        print(crate_stockpile)
+    result = ""
+    for crate in crate_stockpile.crates:
+        result += str(crate[-1])
+    return result
+
+def first_part(data, show_execution: False):
+    return execute_move_orders(data, show_execution, reverse_move_order_when_moving=True)
+
+def second_part(data, show_execution: False):
+    return execute_move_orders(data, show_execution, reverse_move_order_when_moving=False)
 
 input_file_name = "day05.txt"
 
 with open(input_file_name) as input_file:
     data = parse_input(input_file)
-    result_first = first_part(data)
+    result_first = first_part(data, show_execution=False)
     print("Result of the first part is: {}".format(result_first))
 
 with open(input_file_name) as input_file:
     data = parse_input(input_file)
-    result_second = second_part(data)
+    result_second = second_part(data, show_execution=False)
     print("Result of the second part is: {}".format(result_second))
